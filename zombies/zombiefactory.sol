008f7f965edcdc8b3ce4bba9a13b375197e17f93
Trey-Thomas Beattie <beattietrey@gmail.com>
2021-11-04 19:26:11 -0700

    Update factory and add feeding

--- zombies/zombiefactory.sol
+++ zombies/zombiefactory.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.9;
+pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
 
@@ -7,7 +7,6 @@ contract ZombieFactory {
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
-
     struct Zombie {
         string name;
         uint dna;
@@ -15,8 +14,13 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function _createZombie(string memory _name, uint _dna) private {
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string memory _name, uint _dna) internal {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
     }
 
@@ -26,7 +30,9 @@ contract ZombieFactory {
     }
 
     function createRandomZombie(string memory _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
+        randDna = randDna - randDna % 100;
         _createZombie(_name, randDna);
     }
 
